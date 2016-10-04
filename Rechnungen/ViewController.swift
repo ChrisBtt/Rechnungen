a677922ca6db76b0469c93f04066f7b3423543da
ChrisBtt <chris-blattgerste@gmx.de>
2016-10-04 10:09:39 +0200

    Initial Commit

---
+++ Rechnungen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rechnungen
+//
+//  Created by Christoph Blattgerste on 04.10.16.
+//  Copyright © 2016 Christoph Blattgerste. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
